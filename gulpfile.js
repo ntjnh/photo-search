a6ecfe9b22506f835ae052c63db2577b0597b730
Nate J.M <nate@natejonah.com>
2022-12-09 16:42:06 +0000

    Add gulp-clean-css to scss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { dest, parallel, series, src, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
 const browserSync = require("browser-sync").create();
 
 const scss = () => {
   return src("./src/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS())
     .pipe(dest("./build/css"));
 };
 
