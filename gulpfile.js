f872c2d9192f99eb516b934b0d80cbdf796435de
Nate J.M <nate@natejonah.com>
2022-12-09 16:33:30 +0000

    Set up server gulp task for bsync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { dest, parallel, series, src, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
 
 const scss = () => {
   return src("./src/scss/**/*.scss")
@@ -7,7 +8,16 @@ const scss = () => {
     .pipe(dest("./build/css"));
 };
 
+const serve = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+};
+
 module.exports = {
-  default: scss,
-  sass: scss
+  default: series(scss, serve),
+  sass: scss,
+  serve
 }
