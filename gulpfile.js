500a5623fa9f9d198d93f3224123bfce85cd5fc9
Nate J.M <nate@natejonah.com>
2022-12-11 17:49:19 +0000

    Install and set up postcss and tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,22 @@ const browserSync = require("browser-sync").create();
 gulp.task('sass', function() {
   return gulp.src("./src/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./src/css"));
+});
+
+gulp.task('css', function() {
+  const postcss = require("gulp-postcss");
+  const sourcemaps = require("gulp-sourcemaps");
+  const autoprefixer = require("autoprefixer");
+
+  return gulp.src("src/css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer(), require("tailwindcss"), require("postcss-nested") ]))
+    .pipe(sourcemaps.write("."))
     .pipe(cleanCSS())
     .pipe(gulp.dest("./build/css"))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('js', function() {
   return gulp.src(["./src/js/app.js", "./src/js/modules/*.js"])
@@ -38,8 +50,9 @@ gulp.task('serve', function() {
   });
 
   gulp.watch("./src/scss/**/*.scss", gulp.series('sass'));
+  gulp.watch("./src/css/*.css", gulp.series('css'));
   gulp.watch("./index.html").on("change", browserSync.reload);
   gulp.watch("./src/js/**/*.js", gulp.series('js:watch'));
 });
 
-gulp.task('default', gulp.series('sass', 'js'));
+gulp.task('default', gulp.series('sass', 'css', 'js'));
