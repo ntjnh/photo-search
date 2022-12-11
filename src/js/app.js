379930328280e4b42b9b0df70b7e4b00f6304943
Nate J.M <nate@natejonah.com>
2022-12-11 14:15:07 +0000

    Change download to link to unspl
    Also adds close button for photo modal

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,12 @@ import { search } from './modules/search';
 
 const accessKey = key.access;
 search(accessKey);
+
+
+document.querySelector(".close").addEventListener("click", function() {
+  const body = document.getElementsByTagName("body")[0];
+  const photoPanel = document.querySelector(".photo-details");
+
+  body.style.overflow = "auto";
+  photoPanel.style.display = "none";
+});
--- src/js/modules/photoData.js
+++ src/js/modules/photoData.js
@@ -11,6 +11,8 @@ export function photoData(key, photoId) {
     .then(response => response.json())
     .then(data => {
       const {
+        width,
+        height,
         blur_hash: blurHash,
         description,
         alt_description: alt,
@@ -19,13 +21,13 @@ export function photoData(key, photoId) {
           full: urlFull,
         },
         links: {
+          html: photoLink,
           download_location: download
         },
         likes,
         user: {
           username,
           name,
-          location,
           links: {
             html: profile
           },
@@ -33,27 +35,39 @@ export function photoData(key, photoId) {
             large: profilePic
           }
         },
+        location: {
+          name: location
+        },
         views,
         downloads
       } = data;
+
+      // let orientation;
+
+      // if (width > height) {
+      //   orientation = 'landscape';
+      // } else {
+      //   orientation = 'portrait';
+      // }
 
       const photoEl = document.querySelector(".photo");
       photoEl.setAttribute("src", urlReg);
       photoEl.setAttribute("alt", alt);
 
       const descriptionEl = document.querySelector(".description");
-      descriptionEl.textContent = description;
+      descriptionEl.textContent = description ? description : "No description provided.";
+      document.querySelector(".photo-location").textContent = location;
 
       document.querySelector(".photographer img").setAttribute("src", profilePic);
       document.querySelector(".photographer-info .name").textContent = name;
       document.querySelector(".photographer-info .username a").textContent = `@${username}`;
       document.querySelector(".photographer-info .username a").setAttribute("href", profile);
       document.querySelector(".photographer-info .username a").setAttribute("target", "_blank");
-      document.querySelector(".photographer-info .location").textContent = location;
       document.querySelector(".stats .likes").innerHTML = `<span style="font-weight:600;">Likes:</span> ${likes}`;
       document.querySelector(".stats .views").innerHTML = `<span style="font-weight:600;">Views:</span> ${views}`;
       document.querySelector(".stats .downloads").innerHTML = `<span style="font-weight:600;">Downloads:</span> ${downloads}`;
-      document.querySelector(".download a").setAttribute("href", urlFull);
+      document.querySelector(".download a").setAttribute("href", photoLink);
+      document.querySelector(".download a").setAttribute("target", "_blank");
 
     })
     .catch(error => console.error('An error occured while retrieving photo information.', error));
