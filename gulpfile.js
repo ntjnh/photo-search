fadecb4d949379c3a7168fafd03b991881ad0638
Nate J.M <nate@natejonah.com>
2022-12-20 23:18:35 +0000

    Organise gulp tasks to make dev and prod options

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const browserSync = require("browser-sync").create();
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
+const del = require("del");
 
 gulp.task('sass', function() {
   return gulp.src("./src/scss/**/*.scss")
@@ -29,40 +30,77 @@ gulp.task('css', function() {
     .pipe(browserSync.stream())
 })
 
-gulp.task('js', function() {
-  return gulp.src(["./src/js/app.js", "./src/js/modules/*.js"])
-    .pipe(gulpIf(argv.production, webpack({
+function jsStages() {
+  if (argv.production) {
+    return webpack({
       mode: 'production',
       output: {
         filename: 'app.js',
         clean: true
       }
-    }, compiler), webpack({
+    }, compiler);
+  } else {
+    return webpack({
       mode: 'development',
       output: {
         filename: 'app.js',
         clean: false
       }
-    }, compiler)))
+    }, compiler);
+  }
+}
+
+gulp.task('js', function() {
+  return gulp.src(["./src/js/app.js", "./src/js/modules/*.js"])
+    .pipe(jsStages())
     .pipe(gulp.dest("./build/js"))
 });
 
-gulp.task('js:watch', gulp.series('js'), function(done) {
-  browserSync.reload();
+function changeHandler(err, stats, done) {
+  log('[webpack]', stats.toString({
+    colors: true,
+  }));
+
+  browser.reload();
   done();
+}
+
+gulp.task('js:watch', gulp.series('js'), function() {
+  const watchConfig = Object.assign(webpack.config, {
+    watch: true,
+    devtool: 'inline-source-map',
+  });
+
+  return gulp.src(["./src/js/app.js", "./src/js/modules/*.js"])
+    .pipe(webpack(watchConfig, compiler, changeHandler)
+      .on('error', function(err) {
+        log('[webpack:error]', err.toString({
+          colors: true,
+        }));
+      }))
+    .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', function(done) {
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
 
+  done();
+});
+
+gulp.task('watch', function() {
   gulp.watch("./src/scss/**/*.scss", gulp.series('sass'));
   gulp.watch("./src/css/*.css", gulp.series('css'));
   gulp.watch("./index.html").on("change", browserSync.reload);
   gulp.watch("./src/js/**/*.js", gulp.series('js:watch'));
 });
 
-gulp.task('default', gulp.series('sass', 'css', 'js'));
+gulp.task('clean', function() {
+  return del(["build/css/*", "build/js/*"]);
+});
+
+gulp.task('default', gulp.series('sass', 'css', 'js', 'serve', 'watch'));
+gulp.task('build', gulp.series('clean', 'sass', 'css', 'js'));
