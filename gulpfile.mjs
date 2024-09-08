25eeca7bd46471c3e5cddb099c29539063744f3f
Nate J.M <nate@natejonah.com>
2024-09-08 14:43:18 +0100

    Reimplement js environment conditional and tidy up syntax

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,97 +1,117 @@
 import gulp from 'gulp'
+
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 const sass = gulpSass(dartSass)
 import cleanCSS from 'gulp-clean-css'
-import webpack from 'webpack-stream'
-import * as bSync from 'browser-sync'
-const browserSync = bSync.create()
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-const argv = yargs(hideBin(process.argv)).argv
-import { deleteAsync } from 'del'
-
 import postcss from 'gulp-postcss'
 import sourcemaps from 'gulp-sourcemaps'
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
 import postcssNested from 'postcss-nested'
 
+import webpack from 'webpack-stream'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv)).argv
+
+import * as bSync from 'browser-sync'
+const browserSync = bSync.create()
+
+import { deleteAsync } from 'del'
+
 gulp.task('sass', function() {
-  return gulp.src("./src/scss/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./src/css"));
-});
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'))
+})
 
 gulp.task('css', function() {
-  return gulp.src("src/css/*.css")
+  return gulp.src('src/css/*.css')
     .pipe(sourcemaps.init())
-    .pipe(postcss([ autoprefixer(), tailwind({content: ["./index.html"]}), postcssNested ]))
-    .pipe(sourcemaps.write("."))
+    .pipe(postcss([
+      autoprefixer(),
+      tailwind({
+        content: ['./index.html']}),
+        postcssNested
+    ]))
+    .pipe(sourcemaps.write('.'))
     .pipe(cleanCSS())
-    .pipe(gulp.dest("./build/css"))
+    .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream())
 })
 
-
-
-gulp.task('js', function() {
-  return gulp.src(["./src/js/app.js", "./src/js/modules/*.js"])
-    .pipe(webpack({
+function jsStages() {
+  if (argv.production) {
+    return webpack({
       mode: 'production',
       output: {
         filename: 'app.js',
         clean: true
       }
-    }))
-    .pipe(gulp.dest("./build/js"))
-});
+    })
+  } else {
+    return webpack({
+      mode: 'development',
+      output: {
+        filename: 'app.js',
+        clean: false
+      }
+    })
+  }
+}
+
+gulp.task('js', function() {
+  return gulp.src(['./src/js/app.js', './src/js/modules/*.js'])
+    .pipe(jsStages())
+    .pipe(gulp.dest('./build/js'))
+})
 
 function changeHandler(err, stats, done) {
   log('[webpack]', stats.toString({
     colors: true,
-  }));
+  }))
 
-  browser.reload();
-  done();
+  browser.reload()
+  done()
 }
 
 gulp.task('js:watch', gulp.series('js'), function() {
   const watchConfig = Object.assign(webpack.config, {
     watch: true,
-    devtool: 'inline-source-map',
-  });
+    devtool: 'inline-source-map'
+  })
 
-  return gulp.src(["./src/js/app.js", "./src/js/modules/*.js"])
+  return gulp.src(['./src/js/app.js', './src/js/modules/*.js'])
     .pipe(webpack(watchConfig, compiler, changeHandler)
       .on('error', function(err) {
         log('[webpack:error]', err.toString({
-          colors: true,
-        }));
+          colors: true
+        }))
       }))
-    .pipe(gulp.dest("./build/js"));
-});
+    .pipe(gulp.dest('./build/js'))
+})
 
 gulp.task('serve', function(done) {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
-  });
+  })
 
-  done();
-});
+  done()
+})
 
 gulp.task('watch', function() {
-  gulp.watch("./src/scss/**/*.scss", gulp.series('sass'));
-  gulp.watch("./src/css/*.css", gulp.series('css'));
-  gulp.watch("./index.html").on("change", browserSync.reload);
-  gulp.watch("./src/js/**/*.js", gulp.series('js:watch'));
-});
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'))
+  gulp.watch('./src/css/*.css', gulp.series('css'))
+  gulp.watch('./index.html').on('change', browserSync.reload)
+  gulp.watch('./src/js/**/*.js', gulp.series('js:watch'))
+})
 
 gulp.task('clean', function() {
-  return deleteAsync(["build/css/*", "build/js/*"]);
-});
+  return deleteAsync(['build/css/*', 'build/js/*'])
+})
 
-gulp.task('default', gulp.series('sass', 'css', 'js', 'serve', 'watch'));
-gulp.task('build', gulp.series('clean', 'sass', 'css', 'js'));
+gulp.task('default', gulp.series('sass', 'css', 'js', 'serve', 'watch'))
+gulp.task('build', gulp.series('clean', 'sass', 'css', 'js'))
